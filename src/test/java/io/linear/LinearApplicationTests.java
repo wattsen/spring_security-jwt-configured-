725723303262e3a985b8009992f226d898b7936a
baistan.tashkuov <baistan.xi@gmail.com>
2023-03-12 11:03:02 +0600

    Initial commit

---
+++ src/test/java/io/linear/LinearApplicationTests.java
@@ -0,0 +1,13 @@
+package io.linear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
